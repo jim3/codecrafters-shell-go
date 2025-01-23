240c114db8eccc48945bfc5de59164835aaabdfb
jim3 <j3.casey@protonmail.com>
2025-01-23 15:21:22 -0500

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,14 +10,13 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")                                // prompt
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n') // Wait for user input
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found") // strip newline
 	}
-
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
