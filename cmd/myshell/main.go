02d1fc38de6b0ae9574905f5ccee8bfc9abff089
jim3 <j3.casey@protonmail.com>
2025-01-26 18:47:49 -0500

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -14,21 +14,28 @@ const (
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, PROMPT)
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n') // -> (string, error)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-
-		// Split up `command` so that you know what you are executing
 		input := strings.Fields(strings.TrimSpace(command))
+		// Check if there's any input
+		if len(input) > 0 {
+			if input[0] == "exit" && input[1] == "0" {
+				os.Exit(0)
+			}
 
-		// Terminate wtih exit status 0 if the command is `exit 0`
-		if input[0] == "exit" && len(input) == 2 && input[1] == "0" {
-			os.Exit(0)
+			// implement echo cmd
+			if input[0] == "echo" {
+				echoSlice := input[1:]
+				strEcho := strings.Join(echoSlice, " ")
+				echo := strings.TrimSpace(strEcho)
+				fmt.Println(echo)
+			} else {
+				fmt.Println(input[0] + ": command not found")
+			}
 		}
-
-		// Execute the command
-		fmt.Printf("%s: command not found\n", strings.TrimSpace(command))
 	}
 }
